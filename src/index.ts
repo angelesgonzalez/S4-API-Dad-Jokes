4c47439c1784a89687204c553a45188d13e7899b
angelesgonzalez <delosangelesgonzalez@gmail.com>
2025-05-06 13:31:29 +0200

    Create a class Joke to create an instance every time a joke is fetch from an API

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { showJoke, nextJokeButton } from "./services/dom";
+import { createJoke } from "./services/createJokes";
+
 
 document.addEventListener("DOMContentLoaded", (event) => {
 	showJoke();
---
+++ src/services/Joke.ts
@@ -0,0 +1,32 @@
+export class Joke {
+	static nextId = 0;
+	_id: number; 
+	_text: string;
+	_date: Date;
+	_score: number;
+
+	constructor(joke: string) {
+		this._id = Joke.nextId++;
+		this._text = joke;
+		this._date = new Date();
+		this._score = 0;
+	}
+
+	//getters
+	get id() {
+		return this._id;
+	}
+
+	get text() {
+		return this._text;
+	}
+	get date() {
+		return this._date.toISOString();
+    }
+    
+	//setters
+
+	set score(newScore: number) {
+		this._score = newScore;
+	}
+}
