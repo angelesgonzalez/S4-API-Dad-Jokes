8633347665dfd66434f006537fb207b3cb374b93
angelesgonzalez <delosangelesgonzalez@gmail.com>
2025-05-06 10:51:39 +0200

    Add Eventlistener to show the Joke in the DOM

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import { getJoke } from "./services/api";
 
+let updateHTML = (id: string, update: string) => {
+	const htmlElement = document.getElementById(id);
+	if (htmlElement) htmlElement.innerHTML = update;
+};
 
+// let jokeDisplayText = document.getElementById("joke-display__text");
 
-let jokeDisplayText = document.getElementsByClassName("joke-display__text");
+let showJoke = async () => {
+	let joke = await getJoke();
+	if (joke) {
+		updateHTML("joke-display__text", `${joke}`);
+	}
+};
 
-console.log('jokeDisplayText');
+document.addEventListener("DOMContentLoaded", (event) => {
+	showJoke();
+});
